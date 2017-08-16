f2a127aaa049b3e80f53c3d4d402077a157c818f
Daniel Baars <info@danielbaars.nl>
2017-08-16 11:52:33 +0200

    Starter pack upload + D3 example

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# fcc-bar-chart
+# Bar Chart (freeCodeCamp)
+A Bar Chart built as part of [freeCodeCamp](https://www.freecodecamp.com)'s 'Data Visualization Certification', using [D3](https://github.com/d3/d3).
+
+![Screenshot of project](https://danielbaars.github.io/fcc-bar-chart/screenshot.png)
+
+[View this project live](https://danielbaars.github.io/fcc-bar-chart
---
+++ src/components/app.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+import BarChart from './BarChart';
+
+class App extends Component {
+   render() {
+   return (
+      <div className='App'>
+      <div className='App-header'>
+      <h2>d3ia dashboard</h2>
+      </div>
+      <div>
+      <BarChart data={[5,10,1,3,6,8,2,10,12]} size={[500,500]} />
+      </div>
+      </div>
+   )
+   }
+}
+export default App
